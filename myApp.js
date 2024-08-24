40ed8cbe7f11b93a8b25a7aaf7055339c0940c9c
Madjda <m_fares@estin.dz>
2024-08-24 16:45:19 +0100

    Added Helmet middleware to secure Express app

--- myApp.js
+++ myApp.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 
+const helmet = require('helmet');
+app.use(helmet());
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
 
 
